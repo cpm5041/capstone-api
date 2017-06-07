154a50764af062e76a64c2a625bf3fa04fc347e5
cpm5041 <christopher.marcenelle@libertymutual.com>
2017-06-07 15:11:16 -0400

    User Serializer
    Added user serializer to access user email from front end.

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,4 +1,4 @@
 class PetSerializer < ActiveModel::Serializer
-  attributes :id, :species, :breed, :name, :image, :bornOn, :gender
+  attributes :id, :species, :breed, :name, :image, :bornOn, :gender, :user
   has_one :user
 end
